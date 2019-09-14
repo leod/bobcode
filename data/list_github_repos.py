9657800936db40817d577b7b37ef20035791cb7b
leod <subtle.frustration@proton.me>
2019-09-14 21:01:51 +0200

    Use pagination for retrieving github repo list

--- data/list_github_repos.py
+++ data/list_github_repos.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Retrrieve a list of GitHub repositories of a given programming language.
+Retrieve a list of GitHub repositories of a given programming language.
 """
 
 import sys
@@ -12,24 +12,28 @@ from urllib.parse import quote as urlencode
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--min_stars', type=int, default=1000, help='Minimal number of stars.')
 parser.add_argument('--language', required=True, help='Programming language.')
+parser.add_argument('--pages', type=int, required=True, help='Number of pages to retrieve.')
 
 args = parser.parse_args()
 
-url = 'https://api.github.com/search/repositories?q=stars:>={}+language:{}&sort=stars&order=desc' \
+url_prefix = 'https://api.github.com/search/repositories?q=stars:>={}+language:{}&sort=stars&order=desc' \
   .format(args.min_stars, urlencode(args.language))
 
-sys.stderr.write('Getting: {}\n'.format(url))
+for page in range(args.pages):
+  url = url_prefix + '&page={}'.format(page+1)
 
-response = requests.get(url)
+  sys.stderr.write('Getting: {}\n'.format(url))
+  response = requests.get(url)
 
-if response.status_code == 200:
-  obj = json.loads(response.text)
+  if response.status_code == 200:
+    obj = json.loads(response.text)
+    #print(json.dumps(obj, indent=4))
 
-  for item in obj['items']:
-    sys.stdout.write(item['ssh_url'])
-    sys.stdout.write('\n')
+    for item in obj['items']:
+      sys.stdout.write(item['ssh_url'])
+      sys.stdout.write('\n')
 
-  sys.stderr.write('Response is incomplete? {}\n'.format(obj['incomplete_results']))
-else:
-  sys.stderr.write('Got error code {}'.format(response.status_code))
-  sys.exit(1)
+    #sys.stderr.write('Response is incomplete? {}\n'.format(obj['incomplete_results']))
+  else:
+    sys.stderr.write('Got error code {}'.format(response.status_code))
+    sys.exit(1)
