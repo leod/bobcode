fdaefc55801ee7fcd184578aa0767823633e9cd1
leod <subtle.frustration@proton.me>
2019-09-18 17:39:10 +0200

    Allow specifying first page to download

--- data/list_github_repos.py
+++ data/list_github_repos.py
@@ -16,13 +16,14 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('--min_stars', type=int, default=1000, help='Minimal number of stars.')
 parser.add_argument('--language', required=True, help='Programming language.')
 parser.add_argument('--pages', type=int, required=True, help='Number of pages to retrieve.')
+parser.add_argument('--first_page', type=int, default=1, help='First page to retrieve.')
 
 args = parser.parse_args()
 
 url_prefix = 'https://api.github.com/search/repositories?q=stars:>={}+language:{}&sort=stars&order=desc' \
   .format(args.min_stars, urlencode(args.language))
 
-for page in range(args.pages):
+for page in range(args.first_page-1, args.pages):
   url = url_prefix + '&page={}'.format(page+1)
 
   sys.stderr.write('Getting: {}\n'.format(url))
