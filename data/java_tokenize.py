48fdeb51cf2ccd2a81bc4a7815dd6dc460ac02fa
leod <subtle.frustration@proton.me>
2019-09-18 21:14:43 +0200

    Fix bug with missing newline after singleline comment

--- data/java_tokenize.py
+++ data/java_tokenize.py
@@ -28,32 +28,45 @@ def replace_consecutive(s, x, y, y_after):
 
     i += 1
 
+  if start is not None:
+    if not was_newline:
+      new_s += ' '
+    new_s += y * (i - start) + y_after
+
   return new_s
 
-with open(sys.argv[1]) as f:
+def tokenize(s, out):
   prev_line = 1
   prev_column = 1
-  for tok in javalang.tokenizer.tokenize(f.read()):
+
+  for tok in javalang.tokenizer.tokenize(contents):
     num_newlines = tok.position[0] - prev_line
     if num_newlines > 0:
-      sys.stdout.write(NEWLINE_SYMBOL * num_newlines)
-      sys.stdout.write('\n')
+      out.write(NEWLINE_SYMBOL * num_newlines)
+      out.write('\n')
       prev_column = 1
-    prev_line = tok.position[0] + tok.value.count('\n')
+    value = tok.value.strip()
+    prev_line = tok.position[0] + value.count('\n')
 
     num_spaces = tok.position[1] - prev_column
     if num_spaces > 0:
-      sys.stdout.write(SPACE_SYMBOL * num_spaces)
-      sys.stdout.write(' ')
+      out.write(SPACE_SYMBOL * num_spaces)
+      out.write(' ')
     prev_column = tok.position[1] + len(tok.value)
 
     # Join consecutive space symbols or newline symbols into a single word
-    text = tok.value
-
+    text = value
     text = replace_consecutive(text, x=' ', y=SPACE_SYMBOL, y_after=' ')
     text = replace_consecutive(text, x='\n', y=NEWLINE_SYMBOL, y_after='\n')
 
-    sys.stdout.write(text.strip(' '))
-    sys.stdout.write(' ')
+    out.write(text.strip(' '))
+    out.write(' ')
+  out.write(EOF_SYMBOL)
 
-sys.stdout.write(EOF_SYMBOL)
+if __name__ == '__main__':
+  with open(sys.argv[1]) as f:
+    contents = f.read()
+    tokenize(contents, sys.stdout)
+
+    #for tok in javalang.tokenizer.tokenize(contents):
+    #  print("TOK", tok, tok.position)
