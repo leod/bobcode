ca1ba59be9040043b53f053625dc126f7c8ecd75
leod <subtle.frustration@proton.me>
2019-09-19 14:47:34 +0200

    Allow passing file with list of repos to stats script

--- data/repos_stats.sh
+++ data/repos_stats.sh
@@ -1,13 +1,23 @@
 dir=$1
 name=$2
 
-repos=$(ls $dir | wc -l)
-files=$(find $dir -type f -name $name | wc -l)
-read lines words chars \
-  < \
-  <(find $dir -type f -name $name -print0 \
-    | xargs -0 cat \
-    | wc)
+if [ ! -f $dir ]; then
+  repos=$(ls $dir | wc -l)
+  files=$(find $dir -type f -name $name | wc -l)
+  read lines words chars \
+    < \
+    <(find $dir -type f -name $name -print0 \
+      | xargs -0 cat \
+      | wc)
+else
+  repos=$(wc -l < $dir)
+  files=$(find $(< $dir) -type f -name $name | wc -l)
+  read lines words chars \
+    < \
+    <(find $(< $dir) -type f -name $name -print0 \
+      | xargs -0 cat \
+      | wc)
+fi
 
 echo -e "number of repos\t$repos
 number of files\t$files
@@ -15,5 +25,6 @@ number of lines\t$lines
 number of words\t$words
 number of chars\t$chars
 files/repo\t$(bc -l <<< $files/$repos)
+lines/repo\t$(bc -l <<< $lines/$repos)
 lines/file\t$(bc -l <<< $lines/$files)
 words/line\t$(bc -l <<< $words/$lines)"
