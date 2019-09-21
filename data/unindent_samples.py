871bd7493a0ec2dc998268c5977c0de93f3811b9
leod <subtle.frustration@proton.me>
2019-09-21 13:54:41 +0200

    Add script for generating unindented samples
    The purpose of this is to make generating with the model more easy. Without unindented samples, the model would see method implementations _only_ in indented contexts.

---
+++ data/unindent_samples.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import sys
+
+SPACE_SYMBOL = '▁'
+NEWLINE_SYMBOL = '▏'
+
+def prefix_length(char, string):
+  assert len(char) == 1
+
+  length = 0
+  i = 0
+  while i < len(string) and string[i] == char:
+    length += 1
+    i += 1
+
+  return length
+
+def unindent_sample(sample):
+  lines = sample.split(NEWLINE_SYMBOL)
+
+  line_starts = [0]
+  line_ends = []
+  was_newline = False
+
+  for i, c in enumerate(sample):
+    if c != NEWLINE_SYMBOL and was_newline:
+      was_newline = False
+      line_starts.append(i)
+
+      if i != 0:
+        line_ends.append(i)
+    elif c == NEWLINE_SYMBOL:
+      was_newline = True
+
+  line_ends.append(len(sample))
+
+  assert len(line_starts) == len(line_ends)
+
+  indents = [
+    prefix_length(SPACE_SYMBOL, sample[line_starts[i]:line_ends[i]].strip())
+    for i in range(len(line_starts))
+  ]
+
+  if len(indents) <= 1:
+    return None
+
+  min_succ = min(indents[1:])
+
+  if min_succ == 0:
+    return None
+  if indents[0] < min_succ and indents[0] != 0:
+    return None
+
+  result = ''
+  for start, end, indent in zip(line_starts, line_ends, indents):
+    unindent = min_succ if indent >= min_succ else 0
+    result += sample[start:end].strip()[unindent:]
+    result += ' '
+
+  return result + '\n'
+
+if __name__ == '__main__':
+  for sample in sys.stdin:
+    unindented = unindent_sample(sample)
+    if unindented:
+      sys.stdout.write(unindented)
