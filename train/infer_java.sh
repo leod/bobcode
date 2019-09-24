03f7b67de2b31d46f466b434a50e7588874ecf5e
leod <subtle.frustration@proton.me>
2019-09-24 14:54:27 +0200

    Add more infer configs

--- train/infer_java.sh
+++ train/infer_java.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+MODE=${MODE:-sampling4}
+TEST=${TEST:-"$(dirname $0)/../data/work/java/data/test.sample-prefixes.java.pp.bpe"}
+
 name=train_java
 
 CUDA_VISIBLE_DEVICES= onmt-main infer \
   --model $(dirname $0)/model/gpt_2.py \
-  --config $(dirname $0)/train.yml $(dirname $0)/data_java.yml $(dirname $0)/$name.yml $(dirname $0)/infer_sampling30.yml \
-  --features_file $(dirname $0)/../data/work/java/data/test.sample-prefixes.java.pp.bpe
+  --config $(dirname $0)/train.yml $(dirname $0)/data_java.yml $(dirname $0)/$name.yml $(dirname $0)/infer_$MODE.yml \
+  --features_file "$TEST"
