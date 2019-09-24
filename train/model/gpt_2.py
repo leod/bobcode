85c799ed317fc615af82c929eb82acddc259176d
leod <subtle.frustration@proton.me>
2019-09-24 14:54:39 +0200

    Also prepend <s> to features in inference

--- train/model/gpt_2.py
+++ train/model/gpt_2.py
@@ -5,6 +5,8 @@ import numpy as np
 import opennmt as onmt
 import tensorflow as tf
 
+import sys
+
 from opennmt.utils import misc
 
 def gelu(x):
@@ -54,6 +56,15 @@ class GPT2Small(onmt.models.LanguageModel):
             "maximum_features_length": 512
         }
     })
+
+  def _call(self, features, labels, params, mode):
+    #features['ids'] = tf.Print(features['ids'], [tf.shape(features['ids']), features['ids']], summarize=1000)
+    features = {
+        "tokens": tf.concat([[[onmt.constants.START_OF_SENTENCE_TOKEN]], features["tokens"]], 1),
+        "ids": tf.concat([[[onmt.constants.START_OF_SENTENCE_ID]], features["ids"]], 1),
+        "length": features["length"]+1
+    }
+    return super(GPT2Small, self)._call(features, labels, params, mode)
 
 # Special case the language model inputter so that examples do not end with
 # </s>. We need to do this because we train on (almost) fixed size examples of
