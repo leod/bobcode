8f61a1fbf75d21855ebec24b96dcb36d1fb13abe
leod <subtle.frustration@proton.me>
2019-09-25 12:44:12 +0200

    Use own version of OpenNMT-tf for step offset fix

--- train/infer_java.sh
+++ train/infer_java.sh
@@ -5,7 +5,7 @@ TEST=${TEST:-"$(dirname $0)/../data/work/java/data/test.sample-prefixes.java.pp.
 
 name=train_java
 
-CUDA_VISIBLE_DEVICES= onmt-main infer \
+PYTHONPATH=$(dirname $0)/../../OpenNMT-tf CUDA_VISIBLE_DEVICES= python3 $(dirname $0)/../../OpenNMT-tf/opennmt/bin/main.py infer \
   --model $(dirname $0)/model/gpt_2.py \
   --config $(dirname $0)/train.yml $(dirname $0)/data_java.yml $(dirname $0)/$name.yml $(dirname $0)/infer_$MODE.yml \
   --features_file "$TEST"
